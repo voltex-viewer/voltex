a9b2663146ef1f48a64f4f48bfd0698bcf39f83a
Tom Gillbe <git@tgillbe.com>
2025-09-16 11:57:05 -0700

    Fix build error

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config
 export default defineConfig({
   build: {
     sourcemap: true,
     minify: false, // Disable minification for better debugging
+  },
+  resolve: {
+    alias: {
+      '@voltex/plugin-api': path.resolve(__dirname, 'packages/plugin-api/src/index.ts')
+    }
   },
   define: {
     __VUE_OPTIONS_API__: true,
