197c71d348b34471f18095948083d567beaa6b30
Tom Gillbe <git@tgillbe.com>
2025-11-16 17:34:27 -0800

    Add CORS to web config

--- vite.web.config.ts
+++ vite.web.config.ts
@@ -35,7 +35,11 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true
+    open: true,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
   },
   resolve: {
     alias: {
